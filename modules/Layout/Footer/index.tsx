b2749d86ba974ae482f9d533aa151b0a843168e6
devOleksandr <dev.oleksandroliinyk@gmail.com>
2025-01-27 10:56:48 +0200

    footer upd

--- modules/Layout/Footer/index.tsx
+++ modules/Layout/Footer/index.tsx
@@ -45,58 +45,64 @@ export const Footer = () => {
 		e.preventDefault();
 	}
 
+	// TODO: check h t disable outline style f input
 	return (
-		<footer className="w-full bg-white border-t-2 border-gray-light">
-			<div className="container py-[48px] flex items-start">
-				<div className="mr-[72px] max-w-[285px]">
-					<LogoIcon className="max-w-[185px] w-full object-contain mb-4"/>
-					<p className="text-gray-light text-xs"> 400 University Drive Suite 200 Coral Gables, FL 33134 USA</p>
-				</div>
-				<div className="max-w-[185px] mr-[72px]">
-					<p className="text-gray-light mb-6">Links</p>
-					<ul>
-						{
-							linksData.map((item, index) => {
-								return (
-									<li key={index} className="group mb-[36px] last:mb-0">
-										<Link
-											href={item.link}
-											className="group-hover:text-gold-hover text-black font-bold"
-										>{item.label}</Link>
-									</li>
-								)
-							})
-						}
-					</ul>
-				</div>
-				<div className="max-w-[185px] mr-[72px]">
-					<p className="text-gray-light mb-6">Help</p>
-					<ul>
-						{
-							helpData.map((item, index) => {
-								return (
-									<li key={index} className="group mb-[36px] last:mb-0">
-										<Link
-											href={item.link}
-											className="group-hover:text-gold-hover text-black font-bold"
-										>{item.label}</Link>
-									</li>
-								)
-							})
-						}
-					</ul>
-				</div>
-				<form className="flex" onSubmit={onSendHandler}>
-					<div className="max-w-[200px] mr-3 ">
-						<input
-							type="email"
-							className="w-full newsletter-input"
-							value={email}
-							placeholder="Enter Your Email Address"
-							onChange={(e) => setEmail(e.target.value)}/>
+		<footer className="w-full bg-white border-t border-gray-light">
+			<div className="container pt-[48px] ">
+				<div className="w-full flex items-start pb-[48px]">
+					<div className="mr-[72px] max-w-[285px]">
+						<LogoIcon className="max-w-[185px] w-full object-contain mb-4"/>
+						<p className="text-gray-light text-xs"> 400 University Drive Suite 200 Coral Gables, FL 33134 USA</p>
 					</div>
-					<button className="newsletter-button">SUBSCRIBE</button>
-				</form>
+					<div className="max-w-[185px] mr-[72px]">
+						<p className="text-gray-light mb-6">Links</p>
+						<ul>
+							{
+								linksData.map((item, index) => {
+									return (
+										<li key={index} className="group mb-[36px] last:mb-0">
+											<Link
+												href={item.link}
+												className="group-hover:text-gold-hover text-black font-bold"
+											>{item.label}</Link>
+										</li>
+									)
+								})
+							}
+						</ul>
+					</div>
+					<div className="max-w-[185px] mr-[72px]">
+						<p className="text-gray-light mb-6">Help</p>
+						<ul>
+							{
+								helpData.map((item, index) => {
+									return (
+										<li key={index} className="group mb-[36px] last:mb-0">
+											<Link
+												href={item.link}
+												className="group-hover:text-gold-hover text-black font-bold"
+											>{item.label}</Link>
+										</li>
+									)
+								})
+							}
+						</ul>
+					</div>
+					<form className="flex" onSubmit={onSendHandler}>
+						<div className="max-w-[200px] mr-3 ">
+							<input
+								type="email"
+								className="w-full newsletter-input"
+								value={email}
+								placeholder="Enter Your Email Address"
+								onChange={(e) => setEmail(e.target.value)}/>
+						</div>
+						<button className="newsletter-button">SUBSCRIBE</button>
+					</form>
+				</div>
+				<div className="w-full py-[36px] border-t border-gray-light">
+					<p>2023 furino. All rights reverved</p>
+				</div>
 			</div>
 		</footer>
 	)
